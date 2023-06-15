1352cccc013c6da1f9ee590104ef73a6069f3074
Bunty Raghani <bootcamptoprod@gmail.com>
2023-06-16 01:19:12 +0530

    Initial commit

---
+++ src/main/java/com/bootcamptoprod/retry/SpringBootRetryApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamptoprod.retry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRetryApplication.class, args);
+	}
+
+}
